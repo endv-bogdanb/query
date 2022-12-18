bfa522c2d9c537d8dfd20d55ac77fc622c4ff2e2
Bogdan Bosca <109202804+endv-bogdanb@users.noreply.github.com>
2022-12-18 21:50:47 +0200

    chore(vite): add base url /query (#1)

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,10 @@ import WindiCSS from "vite-plugin-windicss";
 import react from "@vitejs/plugin-react";
 import tsconfigPaths from "vite-tsconfig-paths";
 
+const isProduction = process.env.NODE_ENV === "production";
+
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react(), WindiCSS(), tsconfigPaths()],
+  base: isProduction ? `/query/` : undefined,
 });
