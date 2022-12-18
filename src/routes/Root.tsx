7aeeb0234085b596dd3e5d15a372f9d03a1dbe11
Bogdan B <bogdan-dan.bosca@endava.com>
2022-12-18 23:01:07 +0200

    feat(root): add version

--- src/routes/Root.tsx
+++ src/routes/Root.tsx
@@ -1,19 +1,39 @@
+import { useEffect, useState } from "react";
 import { Link, Outlet } from "react-router-dom";
 import { Layout } from "../components/Layout";
 
 export function Root() {
+  const [version, setVersion] = useState<string | null>(null);
+
+  useEffect(() => {
+    const ab = new AbortController();
+
+    fetch("/version.txt", { signal: ab.signal })
+      .then((response) => response.text())
+      .then((txt) => setVersion(txt));
+
+    return () => {
+      ab.abort();
+    };
+  });
+
   return (
     <Layout>
-      <nav className="flex gap-4 h-8 items-center">
-        <ul>
-          <Link to="/query">query</Link>
-        </ul>
-        <ul>
-          <Link to="/swr">swr</Link>
-        </ul>
-        <ul>
-          <Link to="/toolkit-query">toolkit query</Link>
-        </ul>
+      <nav className="flex px-8 justify-between">
+        <div className="flex items-center">
+          {version !== null ? <>version: {version}</> : null}
+        </div>
+        <div className="flex gap-4 h-8 items-center">
+          <ul>
+            <Link to="/query">query</Link>
+          </ul>
+          <ul>
+            <Link to="/swr">swr</Link>
+          </ul>
+          <ul>
+            <Link to="/toolkit-query">toolkit query</Link>
+          </ul>
+        </div>
       </nav>
       <div className="border bg-dark-500" />
       <Outlet />
