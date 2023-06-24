173b84b6ec5469d2f9fa90af0bc5385cfb68001b
Bogdan Bosca <bogdan-dan.bosca@endava.com>
2023-06-24 12:02:28 +0300

    feat(features): update react-query to v4

--- src/features/query/Query.tsx
+++ src/features/query/Query.tsx
@@ -1,5 +1,5 @@
-import { QueryClient, QueryClientProvider } from "react-query";
 import { Outlet } from "react-router-dom";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 
 const client = new QueryClient();
 
